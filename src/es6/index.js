72a6301a3f5b66d20b33467bcdecd54dc694b90d
IvanG <kikin-gr2000@hotmail.com>
2021-12-28 18:28:56 -0600

    promisses agregadas

--- src/es6/index.js
+++ src/es6/index.js
@@ -11,6 +11,13 @@ function newFunction2({ name = 'Oscar', age = 32, country = 'MX' }) {
     console.log(name, age, country);
 }
 
+// function newFunction2( name = 'Oscar', age = 32, country = 'MX' ) {
+//     console.log(name, age, country);
+// }
+
+// newFunction2(); //Tomaria los valores por defecto
+// newFunction2('Ricardo', 19, 'CO');
+
 let pais = 'dsdsd';
 
 let persona = {
@@ -21,16 +28,92 @@ let persona = {
 
 newFunction2(persona);
 
-let frase = `A ver si
-        cierto`;
-console.log(frase); 
+let lorem = 'Quiero escribir una frase super epica \n' + 'Otra frase epica 2';
+console.log(lorem);
+
+//es6
+let lorem2 = `Quiero escribir una frase super epica 
+            Otra frase epica 2`; //Multilinea en ECMASCRIPT6
+console.log(lorem2);
+
+///////////////////////////////////////////
 
 let person = {
     name: 'Kike',
     age: 20,
-    country: 'Mx'
+    'country': 'Mx'
+}
+//console.log(person.name, person.age);
+//es6  Destructuracion de elementos de un objeto
+let { name, age, country } = person; //Tiene que coinicidir los nombres
+console.log(name, age, country);
+
+////////////////////////////////////////////
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+//es6 Propagacion de elementos
+let education = ['David', ...team1, ...team2];
+console.log(education);
+
+////////////////////////////////////////////
+
+let name = 'kike';
+let age = 21;
+//es5
+obj = { name: name, age: age };
+//es6
+obj2 = { name, age };
+console.log(obj2);
+
+///////// ARROW FUNCTIONS ///////////////////
+
+const names = [
+    { name: 'Kike', age: 21 },
+    { name: 'Caro', age: 20 }
+];
+
+//es5
+let listOfNames = names.map(function (item) {
+    console.log(item.name);
+});
+
+//es6
+let listOfNames2 = names.map(item => console.log(item.name));
+
+/////////////// PROMISES /////////////////////
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('hola');
+        } else {
+            reject('Fsota');
+        }
+    });
+}
+helloPromise()
+    .then(Response => console.log(Response))
+    .catch(error => console.log(error));
+
+const promesa2 = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            resolve('resuelta');
+        } else {
+            reject(console.log('No resuelta'));
+        }
+    })
 }
 
-let { name, age } = person;
+promesa2().then(b => console.log('1')).catch(a => console.log('2'));
 
-console.log(name, age);
+////////////////////////////////////////////////
+
+// function diAlgo(frase) {
+//     console.log(frase);
+// }
+
+// setTimeout(() => diAlgo("pasaron 5 segundos"), 5000);
+
+// console.log(`hola bandamaz`);
